e849837735dda70096511b7cfa0cc2556be2c0d8
inesplc <ipalma6@msn.com>
2025-10-14 21:46:51 +0100

    Fix path name

--- scripts/data_sync.sh
+++ scripts/data_sync.sh
@@ -2,4 +2,4 @@
 set -eu
 
 echo "Starting data_sync.sh as $(id -un) (uid=$(id -u), gid=$(id -g))"
-s3cmd --access_key="$CLOUDFLARE_R2_KEY_ID" --secret_key="$CLOUDFLARE_R2_SECRET_KEY" --host="$CLOUDFLARE_ACCOUNT_ID".r2.cloudflarestorage.com --host-bucket="$CLOUDFLARE_ACCOUNT_ID".r2.cloudflarestorage.com sync /app/data/ s3://actual-budget/data/
+s3cmd --access_key="$CLOUDFLARE_R2_KEY_ID" --secret_key="$CLOUDFLARE_R2_SECRET_KEY" --host="$CLOUDFLARE_ACCOUNT_ID".r2.cloudflarestorage.com --host-bucket="$CLOUDFLARE_ACCOUNT_ID".r2.cloudflarestorage.com sync $ACTUAL_DATA_DIR s3://actual-budget/data/
